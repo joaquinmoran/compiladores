7a7fa4034cfab8ed1408845fc344af1c05ee70ce
joaquinmoran <joacomoran6@gmail.com>
2023-10-10 13:42:56 -0300

    better implementation

--- preproyecto/interCodeGen.c
+++ preproyecto/interCodeGen.c
@@ -20,50 +20,54 @@ void declClass(struct  tree *declNode){
 
 }
 
-void exprClass(struct tree *exprNode){
+char *exprClass(struct tree *exprNode){
     char *name = exprNode->left->info.name;
+    char result[10];
     if(strcmp(exprNode->info.name, "PROD") == 0){
-            fprintf(file, "si entro \n");
+        struct tree *right = exprNode;
+        if(strcmp(right->left->info.type, "ID") == 0){
+            if(strcmp(right->right->info.type, "ID") == 0){
+                char *nameLO = right->left->info.name;
+                char *nameRO = right->right->info.name;
+                fprintf(file, "T%d := %s * %s\n", i, nameLO, nameRO);
                 fflush(file);
-                struct tree *right = exprNode;
-                if(strcmp(right->left->info.type, "ID") == 0){
-                    if(strcmp(right->right->info.type, "ID") == 0){
-                        char *nameLO = right->left->info.name;
-                        char *nameRO = right->right->info.name;
-                        fprintf(file, "T%d := %s * %s\n", i, nameLO, nameRO);
-                        fprintf(file, "%s := T%d\n", name, i);
-                        fflush(file);
-                        i++;
-                    }else{
-                        char *nameLO = right->left->info.name;
-                        int val = right->right->info.value;
-                        fprintf(file, "T%d := %s * %d\n", i, nameLO, val);
-                        fprintf(file, "%s := T%d\n", name, i);
-                        fflush(file);
-                        i++;
-                    }
+                sprintf(result, "T%d",i);
+                i++;
+            }else{
+                char *nameLO = right->left->info.name;
+                int val = right->right->info.value;
+                fprintf(file, "T%d := %s * %d\n", i, nameLO, val);
+
+                fflush(file);
+                sprintf(result, "T%d",i);
+                i++;
+            }
+        }else{
+            if(strcmp(right->right->info.type, "ID") == 0){
+                char *nameRO = right->right->info.name;
+                    int val = right->left->info.value;
+                    fprintf(file, "T%d := %d * %s\n", i, val, nameRO);
+                    fflush(file);
+                    sprintf(result, "T%d",i);
+                    i++;
                 }else{
-                    if(strcmp(right->right->info.type, "ID") == 0){
-                        char *nameRO = right->right->info.name;
-                            int val = right->left->info.value;
-                            fprintf(file, "T%d := %d * %s\n", i, val, nameRO);
-                            fprintf(file, "%s := T%d\n", name, i);
-                            fflush(file);
-                            i++;
-                        }else{
-                            int valR = right->right->info.value;
-                            int valL = right->left->info.value;
-                            fprintf(file, "T%d := %d * %d\n", i, valL, valR);
-                            fprintf(file, "%s := T%d\n", name, i);
-                            fflush(file);
-                            i++;
-                        }
+                    int valR = right->right->info.value;
+                    int valL = right->left->info.value;
+                    fprintf(file, "T%d := %d * %d\n", i, valL, valR);
+                    fflush(file);
+                    sprintf(result, "T%d",i);
+                    i++;
                 }
         }
+    }
+    return strdup(result);
 }
 
 void assigClass(struct tree *exprNode){
-
+    bool leftE = false; // para verificar si 
+    bool rightE = false;
+    char *t1;
+    char *t2;
     if(exprNode == NULL){
         return;
     }
@@ -73,45 +77,75 @@ void assigClass(struct tree *exprNode){
         if(strcmp(exprNode->right->info.name, "SUM") == 0){
             struct tree *right = exprNode->right;
             if(strcmp(right->right->info.type, "EXPR") == 0){
-                fprintf(file, "entree1\n");
-                fflush(file);
-                assigClass(right->right);
+                 t2 = exprClass(right->right);
+                 rightE = true;
+                 fflush(file);
             }
             if(strcmp(right->left->info.type, "EXPR") == 0){
-                fprintf(file, "entree2  %s\n ",right->left->info.name);
-                fflush(file);
-                assigClass(right->left);
+                 t1 = exprClass(right->left);
+                 leftE = true;
+                 fflush(file);
             }
             if(strcmp(right->left->info.type, "ID") == 0){
                 if(strcmp(right->right->info.type, "ID") == 0){
                     char *nameLO = right->left->info.name;
                     char *nameRO = right->right->info.name;
-                    fprintf(file, "T%d := %s + %s\n", i, nameLO, nameRO);
-                    fprintf(file, "%s := T%d\n", name, i);
-                    fflush(file);
+                    if(leftE && rightE){
+                        fprintf(file, "T%d := %s + %s\n", i, t1, t2);
+                        fprintf(file, "%s := T%d\n", name, i);
+                        fflush(file);
+                    }else{
+                        fprintf(file, "T%d := %s + %s\n", i, nameLO, nameRO);
+                        fprintf(file, "%s := T%d\n", name, i);
+                        fflush(file);
+                    }
                     i++;
                 }else{
                     char *nameLO = right->left->info.name;
                     int val = right->right->info.value;
-                    fprintf(file, "T%d := %s + %d\n", i, nameLO, val);
-                    fprintf(file, "%s := T%d\n", name, i);
-                    fflush(file);
+                    if(leftE){
+                        fprintf(file, "T%d := %s + %s\n", i, t1, val);
+                        fprintf(file, "%s := T%d\n", name, i);
+                        fflush(file);
+                    }else{
+                        fprintf(file, "T%d := %s + %d\n", i, nameLO, val);
+                        fprintf(file, "%s := T%d\n", name, i);
+                        fflush(file);
+                    }
                     i++;
                 }
             }else{
                 if(strcmp(right->right->info.type, "ID") == 0){
                     char *nameRO = right->right->info.name;
-                        int val = right->left->info.value;
+                    int val = right->left->info.value;
+                    if(rightE){
+                        fprintf(file, "T%d := %s + %s\n", i, val, t2);
+                        fprintf(file, "%s := T%d\n", name, i);
+                        fflush(file);
+                    }else{
                         fprintf(file, "T%d := %d + %s\n", i, val, nameRO);
                         fprintf(file, "%s := T%d\n", name, i);
                         fflush(file);
-                        i++;
+                    }
+                    i++;
                     }else{
                         int valR = right->right->info.value;
                         int valL = right->left->info.value;
-                        fprintf(file, "T%d := %d + %d\n", i, valL, valR);
-                        fprintf(file, "%s := T%d\n", name, i);
-                        fflush(file);
+                        if(rightE){
+                            fprintf(file, "T%d := %d + %s\n", i, valL, t2);
+                            fprintf(file, "%s := T%d\n", name, i);
+                            fflush(file);
+                        }else{
+                            if(leftE){
+                                fprintf(file, "T%d := %s + %d\n", i, t1, valR);
+                                fprintf(file, "%s := T%d\n", name, i);
+                                fflush(file);
+                            }else{
+                                fprintf(file, "T%d := %d + %d\n", i, valL, valR);
+                                fprintf(file, "%s := T%d\n", name, i);
+                                fflush(file);
+                            }
+                        }
                         i++;
                     }
             }
@@ -152,8 +186,6 @@ void assigClass(struct tree *exprNode){
                     }
             }
         }
-    }else{
-        exprClass(exprNode);
     }
 }
 
