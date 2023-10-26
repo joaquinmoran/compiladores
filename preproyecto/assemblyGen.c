b7c9de8182602d507d46bf394c966dd644aa117d
joaquinmoran <joacomoran6@gmail.com>
2023-10-26 17:05:47 -0300

    assembly generation first steps

--- preproyecto/assemblyGen.c
+++ preproyecto/assemblyGen.c
@@ -8,10 +8,27 @@
 
 
 extern struct listNode *instrListHead;
-
+FILE *assemblyFile;
 
 
 void listTraverse(){
+    struct listNode *currentNode = instrListHead;
+    assemblyFile = fopen("assemblyGen.s","w");
+    if(assemblyFile == NULL){
+        fprintf(stderr, "Cant open file.\n");
+        return; 
+    }
+    fprintf(assemblyFile, "    .text\n");
+    fprintf(assemblyFile, "    .globl  main\n");
+    fprintf(assemblyFile, "    .type	main, @function\n");
+    fprintf(assemblyFile, "main:\n");
+    fprintf(assemblyFile, "    pushq    %rbp\n");
+    fprintf(assemblyFile, "    movq	    %rsp, %rbp\n");
+    fprintf(assemblyFile, "    subq	    $24, %rsp\n");
+    fflush(assemblyFile);
+    while(currentNode->next != NULL){
+        printf("%s\n", currentNode->next->instr);
+        currentNode = currentNode->next;
+    }
 
-    
 }
